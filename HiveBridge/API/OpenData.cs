9f0e118628664107b437a0489d8300c51bc551cd
Pramuditha Pothuwila <pramudithapothuwila36@gmail.com>
2023-02-22 13:51:54 +0530

    Update OpenData.cs

--- HiveBridge/API/OpenData.cs
+++ HiveBridge/API/OpenData.cs
@@ -4,16 +4,16 @@ public static class OpenData
 {
     public static void ConfigureOpenDataApi(this WebApplication app)
     {
-        app.MapGet("/api/TestConnection", TestConnection).WithName("TestConnection");
-        app.MapGet("/api/TestAuthentication", TestAuthentication).WithName("TestAuthentication").RequireAuthorization();
+        app.MapGet("/api/TestConnection", TestConnectionAsync).WithName("TestConnection");
+        app.MapGet("/api/TestAuthentication", TestAuthenticationAsync).WithName("TestAuthentication").RequireAuthorization();
     }
 
-    private static Task<IResult> TestConnection()
+    private static Task<IResult> TestConnectionAsync()
     {
         return Task.FromResult(Results.Ok());
     }
     
-    private static Task<IResult> TestAuthentication()
+    private static Task<IResult> TestAuthenticationAsync()
     {
         Results.Text("Authenticated");
         return Task.FromResult(Results.Ok());
