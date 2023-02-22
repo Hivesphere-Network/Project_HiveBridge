07d9893ab24b9d40f47fc68fcaf9537509d6c581
Pramuditha Pothuwila <pramudithapothuwila36@gmail.com>
2023-02-22 13:10:15 +0530

    TestAuthentication Endpoint Added

--- HiveBridge/API/OpenData.cs
+++ HiveBridge/API/OpenData.cs
@@ -5,10 +5,17 @@ public static class OpenData
     public static void ConfigureOpenDataApi(this WebApplication app)
     {
         app.MapGet("/api/TestConnection", TestConnection).WithName("TestConnection");
+        app.MapGet("/api/TestAuthentication", TestAuthentication).WithName("TestAuthentication").RequireAuthorization();
     }
 
     private static Task<IResult> TestConnection()
     {
+        return Task.FromResult(Results.Ok());
+    }
+    
+    private static Task<IResult> TestAuthentication()
+    {
+        Results.Text("Authenticated");
         return Task.FromResult(Results.Ok());
     }
 
